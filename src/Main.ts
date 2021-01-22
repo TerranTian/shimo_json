2ced2ed8eda0505fa5004060021fed234358336c
terran tian <terran.tian@foxmail.com>
2021-01-22 14:33:38 +0800

    sync:Auto update

--- src/Main.ts
+++ src/Main.ts
@@ -2,6 +2,7 @@
 import * as path from "path";
 import * as fs from "fs";
 import { parse_shimo } from "./Shimo";
+import { exit } from "process";
 
 let dir = path.dirname(process.argv[1]);
 let parameters = process.argv.splice(2);
@@ -34,14 +35,12 @@ while(parameters.length > 0){
             break;
         case "-t":
         case "--typeRow":
-            typeRow = +parameters.shift()||0;
+            typeRow = +parameters.shift()||1;
             break;
-
         case "-d":
         case "--dataRow":
-            dataRow = +parameters.shift()||0;
+            dataRow = +parameters.shift()||1;
             break;
-
         case "-f":
         case "--format":
             format = parameters.shift() || "common";
@@ -55,12 +54,24 @@ while(parameters.length > 0){
     }
 }
 
+if(!cookie || !fileId || !out){
+    let message = `
+    wrong arguments:
+    -c/--cookie: copy from one of shimo's request.
+    -i/--fileId: shimo's document id: https://shimo.im/sheets/<docment id>/MODOC
+    -n/--nameRow: the index of row for name, default:0
+    -t/--typeRow: the index of row for type, default:1
+    -d/--dataRow: the index of row for data, default:1
+    -f/--format: the json format, default:common, which with full key,[{key1:value1,key2:value2},{key1:value1,key2:value2}], otherwise [keys:[key1,key2],values:[[value1,value2],[value1,value2]]]
+    -o/--out: out path
+`
+    console.log(message);
+    exit(1);
+}
+
 !async function(){
     let data = await parse_shimo(fileId,cookie,{nameRow:+nameRow,typeRow:+typeRow,dataRow:+dataRow})
     if(format == "common"){
-        delete data.types;
-        fs.writeFileSync(out,JSON.stringify(data));
-    }else{
         let contents = [];
         data.values.forEach(v=>{
             let item = {};
@@ -70,6 +81,9 @@ while(parameters.length > 0){
             contents.push(item);
         })
         fs.writeFileSync(out,JSON.stringify(contents));
+    }else{
+        delete data.types;
+        fs.writeFileSync(out,JSON.stringify(data));
     }
-};
+}();
 
